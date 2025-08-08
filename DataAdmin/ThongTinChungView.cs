a3031e208faa8fa1368c6c49edce514aa188b2eb
Nguyen Thanh Phat <23042004phat@gmail.com>
2025-08-08 10:21:42 +0700

    list sân

---
+++ DataAdmin/ThongTinChungView.cs
@@ -0,0 +1,10 @@
+﻿using Frontend.Models;
+
+namespace Frontend.DataAdmin
+{
+    public class ThongTinChungView
+    {
+        public List<SanModel> ListSan { get; set; } = new List<SanModel>();
+        public SanModel San { get; set; } = new SanModel();
+    }
+}
---
+++ Models/SanModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Frontend.Models
+{
+    public class SanModel
+    {
+        public int MaSan { get; set; }
+        public string TenSan { get; set; }
+        public string KieuSan { get; set; }
+        public string TrangThai     { get; set; }
+        public string ViTri {  get; set; }
+    }
+}
